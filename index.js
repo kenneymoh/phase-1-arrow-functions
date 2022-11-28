9a81cb1c7d050afdfa1f726782afbd60e00af9d4
kennedy maina <kenneyrit@gmail.com>
2022-11-28 10:44:21 +0300

    Updated function expression

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const divide = (num1, num2) => num1 / num2;
-divide(2000,100);
+function divide(){
+    let divide =(2000/ 100);
+    return divide;
+}
+divide();
+// const divide = (num1, num2) => num1 / num2;
+// divide(2000,100);
+// console.log(divide());
 
 const square = x => x **2;
 square(2);
+console.log(square());
 
 const add = (a, b) => a +b;
 add(3, 4);
+console.log(add());
